1f2ffa17f2b48b116a4ffca362cf3760cca87f57
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2021-11-21 22:17:19 +0300

    add

---
+++ scripy.js
@@ -0,0 +1,16 @@
+const someFn = "пиТе";  
+ 
+function change_register(someFn) {
+let new_someFn = "";
+let len = someFn.length;
+  for (var i = 0; i < len; i++) {
+    if (someFn[i] === str[i].toLowerCase()) {
+      new_someFn += someFn[i].toUpperCase();
+    } else {
+      new_someFn += someFn[i].toLowerCase();
+    }
+  }
+  return new_someFn;
+}
+
+document.writeln(change_register(someFn)); 
